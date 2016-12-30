cda96058df66a7172903f87dc4fbcb95004c563e
lordel <lorenzo.delcroix@gmail.com>
2016-12-30 12:21:14 +0100

    Added javadoc comments to CharacterGUIController class and edited some comments in both CharacterGUIController and GUIController classes. removed some unnecessary whitespaces. Added some TODOs for more features that still need to be implemented later on.

--- src/com/roleplay/utils/GUIController.java
+++ src/com/roleplay/utils/GUIController.java
@@ -6,6 +6,7 @@ import com.roleplay.Main;
 /**
  * File created by Lorenzo Delcroix on 12/23/2016 2:11.
  * Belongs to the package com.roleplay of the RoleplayHelper project.
+ * //TODO: remove character and rework all dependent classes with get/set methods from Main
  */
 public abstract class GUIController {
     protected Character character;
