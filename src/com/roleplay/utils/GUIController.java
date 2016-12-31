1e61c23284cce6c0c7bd4b415c8817a79d0fb78e
lordel <lorenzo.delcroix@gmail.com>
2016-12-31 14:58:47 +0100

    Added javadoc comments to the classes in the Utils package. Changed a few comments for clarity. Added TODOs for things to be addressed at a later time.

--- src/com/roleplay/utils/GUIController.java
+++ src/com/roleplay/utils/GUIController.java
@@ -4,9 +4,11 @@ import com.roleplay.Character;
 import com.roleplay.Main;
 
 /**
- * File created by Lorenzo Delcroix on 12/23/2016 2:11.
- * Belongs to the package com.roleplay of the RoleplayHelper project.
- * //TODO: remove character and rework all dependent classes with get/set methods from Main
+ * Abstract class providing the basic framework for Controllers used in this project.
+ * This class is used to ensure all Controllers in the project have a common structure which can be used to access Main
+ * and its methods from within the controller. It also enables Main to refer to the general class GUIController when
+ * initializing all Controller classes.
+ * TODO: remove character and rework all dependent classes with get/set methods from Main
  */
 public abstract class GUIController {
     protected Character character;
@@ -16,6 +18,12 @@ public abstract class GUIController {
         this.character = character;
     }
 
+    /**
+     * Sets the main class for this GUIController.
+     * This method is used to store the mainClass as a protected field which can late be used by this class to access
+     * Main's methods.
+     * @param mainClass The Main class instance to be stored.
+     */
     public void setMainClass(Main mainClass) {
         this.mainClass = mainClass;
     }
