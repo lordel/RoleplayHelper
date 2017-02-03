e20ebcddfb74f2dd44363a7e65ccd916195767ca
lordel <lorenzo.delcroix@gmail.com>
2017-02-03 11:26:38 -0600

    Added TODO point

--- src/com/roleplay/package-info.java
+++ src/com/roleplay/package-info.java
@@ -3,5 +3,6 @@
  * TODO: go through all class attributes and reorder them in a more logical manner
  * TODO: go through code to make sure it all fits the right size and conventions
  * TODO: check each IOEXCEPTION throwing method to possibly implement something similar to the continuePressed() method in ValueSettingGUIController
+ * TODO: Add CSS descriptions in each CSS file
  */
 package com.roleplay;
